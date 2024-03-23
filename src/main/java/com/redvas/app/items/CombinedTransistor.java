ac2015b5717b062798e96d8e01aaafe9e928300d
Gosztonyi Olivér <gosztonyi.oliver@gmail.com>
2024-03-23 17:45:41 +0100

    basic methos

--- src/main/java/com/redvas/app/items/CombinedTransistor.java
+++ src/main/java/com/redvas/app/items/CombinedTransistor.java
@@ -1,12 +1,33 @@
 package com.redvas.app.items;
 
+import com.redvas.app.players.Player;
+
 public class CombinedTransistor extends Item{
+
+    @Override
+    public void use(){
+        // use
+    }
+
+    @Override
+    public void pickup(Player who) {
+        super.pickup(who);
+    }
+
+    @Override
+    public void dispose() {
+        // dispose
+    }
+
+    private void setDisposedPair(Transistor transistor){
+        // something
+    }
 
     /**
      * @return Az Item neve
      */
     @Override
     public String toString() {
-        return "összekapcsolt tranzisztor";
+        return "Combined Transistor";
     }
 }
--- src/main/java/com/redvas/app/items/Transistor.java
+++ src/main/java/com/redvas/app/items/Transistor.java
@@ -1,11 +1,22 @@
 package com.redvas.app.items;
 
 public class Transistor extends Item {
+    @Override
+    public void merge(Transistor item){
+        Item tmp = new CombinedTransistor();
+        tmp.setOwner(owner());
+        owner().addToInventory(tmp);
+        destroy();
+        item.destroy();
+        logger.fine("Merged transistor");
+    }
+
+
     /**
      * @return Az Item neve
      */
     @Override
     public String toString() {
-        return "tranzisztor";
+        return "Transistor";
     }
 }
