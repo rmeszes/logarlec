aa19b21e7bdde994326f54dc3aa42bcb7d34fb81
Meszes Richárd <rmeszes1@gmail.com>
2024-03-24 15:32:56 +0100

    Skeleton runner complete, just needs the test cases

--- src/main/java/com/redvas/app/skeleton/Helper.java
+++ src/main/java/com/redvas/app/skeleton/Helper.java
@@ -1,4 +1,67 @@
 package com.redvas.app.skeleton;
 
+import com.redvas.app.players.*;
+import com.redvas.app.map.*;
+import com.redvas.app.items.*;
+import com.redvas.app.*;
+
+
 public class Helper {
+
+    public static void useCase1() {
+        System.out.println("Use case 1: Stepping into another Room:\n");
+
+    }
+
+    public static void useCase2() {
+        System.out.println("use case 2 runs");
+    }
+
+    public static void useCase3() {
+        System.out.println("use case 3 runs");
+    }
+
+    public static void useCase4() {
+        System.out.println("use case 4 runs");
+    }
+
+    public static void useCase5() {
+        System.out.println("use case 5 runs");
+    }
+
+    public static void useCase6() {
+        System.out.println("use case 6 runs");
+    }
+
+    public static void useCase7() {
+        System.out.println("use case 7 runs");
+    }
+
+    public static void useCase8() {
+        System.out.println("use case 8 runs");
+    }
+
+    public static void useCase9() {
+        System.out.println("use case 9 runs");
+    }
+
+    public static void useCase10() {
+        System.out.println("use case 10 runs");
+    }
+
+    public static void useCase11() {
+        System.out.println("use case 11 runs");
+    }
+
+    public static void useCase12() {
+        System.out.println("use case 12 runs");
+    }
+
+    public static void useCase13() {
+        System.out.println("use case 13 runs");
+    }
+
+    public static void useCase14() {
+        System.out.println("use case 14 runs");
+    }
 }
