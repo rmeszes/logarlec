0ed24390c38494dd78357d722e149b54c1e31bf0
Meszes Richárd <rmeszes1@gmail.com>
2024-04-16 12:36:44 +0200

    wetwipe implement proximitylistener

--- src/main/java/com/redvas/app/items/WetWipe.java
+++ src/main/java/com/redvas/app/items/WetWipe.java
@@ -1,6 +1,12 @@
 package com.redvas.app.items;
 
-public class WetWipe extends Item {
+import com.redvas.app.players.Janitor;
+import com.redvas.app.players.Player;
+import com.redvas.app.players.ProximityListener;
+
+import java.util.List;
+
+public class WetWipe extends Item implements ProximityListener {
     /** gives protection from profs
      *
      */
@@ -26,5 +32,35 @@ public class WetWipe extends Item {
     @Override
     public String toString() {
         return "Wet Wipe";
+    }
+
+    @Override
+    public void proximityChanged(Player newcomer) {
+
+    }
+
+    @Override
+    public void proximityEndOfRound(List<Player> proximity) {
+
+    }
+
+    @Override
+    public void proximityInitially(List<Player> proximity) {
+
+    }
+
+    @Override
+    public int listenerPriority() {
+        return 0;
+    }
+
+    @Override
+    public void getAffected(Janitor by) {
+
+    }
+
+    @Override
+    public void getAffected(AirFreshener by) {
+
     }
 }
