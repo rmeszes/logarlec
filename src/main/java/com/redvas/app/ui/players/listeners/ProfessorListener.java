71acc303479f56e59007722f43721b2b0583f844
Meszes Richárd <rmeszes1@gmail.com>
2024-05-03 19:15:36 +0200

    views

---
+++ src/main/java/com/redvas/app/ui/players/listeners/ProfessorListener.java
@@ -0,0 +1,5 @@
+package com.redvas.app.ui.players.listeners;
+
+public interface ProfessorListener {
+    void paralyzedChanged(boolean isParalyzed);
+}
---
+++ src/main/java/com/redvas/app/ui/players/listeners/UndergraduateListener.java
@@ -0,0 +1,5 @@
+package com.redvas.app.ui.players.listeners;
+
+public interface UndergraduateListener {
+    void droppedOut();
+}
