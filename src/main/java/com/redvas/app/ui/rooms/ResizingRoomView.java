286ba247611b38f025b6b67155f4249328b2afa5
Patrik Nusszer <patrik.nusszer@icloud.com>
2024-05-21 23:44:53 +0200

    bbb

--- src/main/java/com/redvas/app/ui/rooms/ResizingRoomView.java
+++ src/main/java/com/redvas/app/ui/rooms/ResizingRoomView.java
@@ -14,19 +14,6 @@ import java.util.logging.Logger;
 
 public class ResizingRoomView extends RoomView implements ResizingRoomChangeListener {
     private static final Logger logger = App.getConsoleLogger(ResizingRoomView.class.getName());
-    private boolean isSticky = false;       // alapból legyen hamis, de am ez nem int a modellben??
-    private boolean isGaseous = false;
-
-    BufferedImage rhFloorImage;
-    BufferedImage rhFloorImageWhenGaseous;
-    BufferedImage rhFloorImageWhenSticky;
-    BufferedImage rhFloorImageWhenGaseousAndSticky;
-
-    // EZT MÉG NEM TOM HOGY DÖNTI EL
-    BufferedImage rvFloorImage;
-    BufferedImage rvFloorImageWhenGaseous;
-    BufferedImage rvFloorImageWhenSticky;
-    BufferedImage rvFloorImageWhenGaseousAndSticky;
 
     private ResizingRoom rr;
     public ResizingRoomView(ResizingRoom rr, int x, int y) {
